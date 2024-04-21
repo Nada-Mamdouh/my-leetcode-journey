ef0efde2edb3899449d26a59619294a0fb5f40fe
Nada-Mamdouh <nada-mamdouh@outlook.com>
2024-04-21 23:54:57 +0200

    Time: 62 ms (86.31%), Space: 42.4 MB (71.08%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cs
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cs
@@ -1,14 +1,7 @@
 public class Solution {
     public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
-        StringBuilder w1_sp = new StringBuilder();
-        StringBuilder w2_sp = new StringBuilder();
-        
-        for(int i = 0;i<word1.Length;i++){
-            w1_sp.Append(word1[i]);
-        }
-        for(int j = 0;j<word2.Length;j++){
-            w2_sp.Append(word2[j]);
-        }
-        return w1_sp.Equals(w2_sp);
+        string s1 = string.Concat(word1);
+        string s2 = string.Concat(word2);
+        return s1.Equals(s2);
     }
 }
