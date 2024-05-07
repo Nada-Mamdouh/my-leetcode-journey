f94a5e6141e1b3feba0d380c37bc89de5e200a5e
Nada-Mamdouh <nada-mamdouh@outlook.com>
2024-05-08 01:28:35 +0300

    Time: 59 ms (74.14%), Space: 43.7 MB (76.68%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -0,0 +1,16 @@
+using System.Text.RegularExpressions;
+
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int start = 0, end = s.Length-1;
+        while(start < end){
+            if(!char.IsLetterOrDigit(s[start])) start++;
+            else if(!char.IsLetterOrDigit(s[end])) end--;
+            else{
+                if(char.ToLower(s[start++]) 
+                != char.ToLower(s[end--])) return false;
+            }
+        }
+       return true;
+    }
+}
