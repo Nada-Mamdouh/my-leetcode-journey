a9c5c1b836d531d8580a5e26b00659bc8fd9c394
Nada-Mamdouh <nada-mamdouh@outlook.com>
2024-05-18 19:51:55 +0300

    Time: 95 ms (49.55%), Space: 46.8 MB (67.86%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cs
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cs
@@ -11,10 +11,9 @@
  */
 public class Solution {
     public bool HasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
         if(head == null || head.next == null) return false;
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
+        while(fast.next != null && fast.next.next != null){
             fast = fast.next.next;
             slow = slow.next;
             if(slow == fast) return true;
