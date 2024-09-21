bb560181aca5488d31399111eb86ffa80e7bf8ad
Nada-Mamdouh <nada-mamdouh@outlook.com>
2024-09-21 23:17:14 +0300

    Time: 85 ms (44.32%), Space: 45.7 MB (93.88%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    bool flag = true;
+    public bool IsBalanced(TreeNode root) {
+        if(root == null) return true;
+        int leftHeight = calcDepth(root.left);
+        int rightHeight = calcDepth(root.right);
+        if(Math.Abs(leftHeight - rightHeight) <= 1 && flag == true) return true;
+        return false;
+    }
+    public int calcDepth(TreeNode root){
+        if(root == null) return 0;
+        int left = calcDepth(root.left);
+        int right = calcDepth(root.right);
+        
+        if(!(Math.Abs(left - right) <= 1)) flag = false;
+        return 1 + Math.Max(left, right);
+    }
+}
