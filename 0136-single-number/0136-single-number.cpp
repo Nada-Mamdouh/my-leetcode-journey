4e580780eb8fb5c93c76d4cf80374d17b8e217bc
Nada Mamdouh <nada-mamdouh@outlook.com>
2024-12-19 00:28:09 +0200

    Time: 7 ms (22.55%), Space: 24.6 MB (5.77%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       //Brute force
-       //TC: O(n*n) sc: O(1)
-       int n = nums.size(),ans = 0, count = 0;
-       for(int i = 0;i<n;i++){
-           count = 0;
-           for(int j = 0;j<n;j++){
-               if(nums[i] == nums[j]){
-                   count++;
-               }
-           }
-
-           if(count == 1){
-               ans = nums[i];
-               break;
-           }
-       }
+        //Better solution
+        //where m is the size of the map which is equal to n/2 + 1;
+        //TC: O(nlogm) + O(n/2 + 1)  - SC: O(n/2 + 1)
+        int n = nums.size(), ans = 0;
+        map<int, int> mpp;
+        for(int i = 0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second == 1){
+                ans = it.first;
+                break;
+            }
+        }
         return ans;
+        
     }
 };
