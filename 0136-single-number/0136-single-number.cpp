b7a41a65f53db074cf0848635d3d7a77aaf01497
Nada Mamdouh <nada-mamdouh@outlook.com>
2024-12-19 00:31:04 +0200

    Time: 0 ms (100.00%), Space: 20.6 MB (35.13%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //Better solution
-        //where m is the size of the map which is equal to n/2 + 1;
-        //TC: O(nlogm) + O(n/2 + 1)  - SC: O(n/2 + 1)
-        int n = nums.size(), ans = 0;
-        map<int, int> mpp;
+        //Optimal solution
+        //TC: O(n) - SC: O(1)
+        int xor1 = 0, n = nums.size();
         for(int i = 0;i<n;i++){
-            mpp[nums[i]]++;
+            xor1 ^= nums[i];
         }
-        for(auto it:mpp){
-            if(it.second == 1){
-                ans = it.first;
-                break;
-            }
-        }
-        return ans;
+        return xor1;
         
     }
 };
