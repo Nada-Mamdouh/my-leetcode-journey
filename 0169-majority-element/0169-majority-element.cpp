1762ad4a20f91003e8df16f0d5c2b675f592c1d6
Nada Mamdouh <nada-mamdouh@outlook.com>
2024-12-23 00:25:28 +0200

    Time: 0 ms (100.00%), Space: 28 MB (51.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
+        map<int, int> mpp;
         for(int i = 0;i<n;i++){
-            int cnt = 0;
-            for(int j = 0;j<n;j++){
-                if(nums[i] == nums[j]) cnt++;
-            }
-            if(cnt > (n/2)) return nums[i];
+            mpp[nums[i]]++;
+        }
+        for(auto ele:mpp){
+            if(ele.second > (n/2)) return ele.first;
         }
         return -1;
-    }
+    } 
 };
