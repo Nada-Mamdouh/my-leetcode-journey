1a06f5c5c1674878e00c1ae4dcb1cadd9fd698d2
Nada Mamdouh <nada-mamdouh@outlook.com>
2024-12-27 00:01:03 +0200

    Time: 3 ms (20.73%), Space: 71.7 MB (43.80%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // Optimal solution 
-        // Applying kadane's algorithm 
-        // TC: O(n), SC: O(1)
+        //Optimal Solution
+        //TC: O(n) - SC: O(1)
         int n = nums.size();
-        int sum = 0;
-        int maxi = nums[0];
+        int maxi = INT_MIN, sm = 0;
         for(int i = 0;i<n;i++){
-            sum += nums[i];
-            if(sum > maxi) maxi = sum;
-            if(sum < 0) sum = 0;
+            sm += nums[i];
+            maxi = max(maxi, sm);
+            if(sm < 0) sm = 0;
         }
         return maxi;
     }
