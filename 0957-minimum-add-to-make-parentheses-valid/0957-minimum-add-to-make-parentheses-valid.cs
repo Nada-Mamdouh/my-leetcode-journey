cc553e7f4968331a6db4184dc9309b624f831a5f
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-03-16 07:38:11 +0200

    Time: 2 ms (30.15%), Space: 39.6 MB (46.56%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cs
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cs
@@ -1,13 +1,14 @@
 public class Solution {
     public int MinAddToMakeValid(string s) {
+        //TC: O(n) - O(n) -> O(2n)
+        //SC: O(n)
         Stack<char> st = new Stack<char>();
         int ans = 0;
         for(int i = 0;i<s.Length;i++){
-            if(st.Count == 0 || (st.Count > 0 && s[i] == ')' && st.Peek() != '(') || s[i] == '('){
-                st.Push(s[i]);
-            }
             if(st.Count > 0 && s[i] == ')' && st.Peek() == '('){
                 st.Pop();
+            }else{
+                st.Push(s[i]);
             }
         }
         while(st.Count > 0){
