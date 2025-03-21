015154c9c5519208b2d3369d9f1ad3a754536ae3
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-03-21 22:49:17 +0200

    Time: 2 ms (60%), Space: 56.3 MB (25.38%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cs
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cs
@@ -1,28 +1,25 @@
 public class Solution {
     public int MaxScore(int[] cardPoints, int k) {
-        int n = cardPoints.Length;
-        int totalSum = cardPoints.Sum();
-        
-        // Edge case: If we need to take all cards
-        if (n == k) return totalSum;
+        //This can be solved in 2 ways:
+        //1- sliding window    2- prefix sum
+        //Let's go with sliding window:
 
-        // Find the minimum sum of a subarray of length (n-k)
-        int windowSize = n - k;
-        int windowSum = 0, minWindowSum;
+        //TC: O(2k)
+        //SC: O(1)
 
-        // Compute the initial window sum
-        for (int i = 0; i < windowSize; i++) {
-            windowSum += cardPoints[i];
+        int leftSum = 0, rightSum = 0, maxPoints = 0;
+        for(int j = 0;j<k;j++){
+            leftSum += cardPoints[j];
         }
-        minWindowSum = windowSum;
-
-        // Slide the window across the array
-        for (int i = windowSize; i < n; i++) {
-            windowSum += cardPoints[i] - cardPoints[i - windowSize];
-            minWindowSum = Math.Min(minWindowSum, windowSum);
+        Console.WriteLine(leftSum);
+        maxPoints = leftSum;
+        int rightIdx = cardPoints.Length - 1;
+        for(int g = k-1; g >= 0;g--){
+            leftSum -= cardPoints[g];
+            rightSum += cardPoints[rightIdx];
+            maxPoints = Math.Max(maxPoints, leftSum + rightSum);
+            rightIdx--;
         }
-
-        // Maximum score is total sum minus the minimum window sum
-        return totalSum - minWindowSum;
+        return maxPoints;
     }
 }
