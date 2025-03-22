81a53cc4b9ebb3c7485f505b79a34dbd90a4ad12
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-03-22 20:31:33 +0200

    Time: 4 ms (95.18%), Space: 44.2 MB (44.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -1,19 +1,21 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        int high = 0;
-        int longestSubString = 0;
-        for (int i = 0; i < s.Length; i++)
-        {
-            HashSet<char> arr = new HashSet<char>();
-            high = i;
-            while(high < s.Length && !arr.Contains(s[high]))
-            {
-                arr.Add(s[high]);
-                high++;
-            }
-            longestSubString = Math.Max(longestSubString, (high - i));
+        int[] arr = new int[256];
+        for(int i = 0;i<arr.Length;i++){
+            arr[i] = -1;
         }
-
-        return longestSubString;
+        int l = 0, r = 0, maxLen = 0, n = s.Length;
+        while(r < n){
+            if(arr[s[r]] != -1){
+                if(arr[s[r]] >= l){
+                    l = arr[s[r]] + 1;
+                }
+            }
+            int length = r - l + 1;
+            maxLen = Math.Max(maxLen, length);
+            arr[s[r]] = r;
+            r++;
+        }
+        return maxLen;
     }
 }
