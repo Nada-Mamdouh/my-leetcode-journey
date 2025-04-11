3e4a79f62e61a1276fd0109ec54b3d925cb62f85
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-04-11 22:21:25 +0200

    Time: 2 ms (87.41%), Space: 43.1 MB (89.21%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        int l = 0,r = 0, maxLen = 0, maxFreq = 0;
+        int[] hash = new int[26];
+        while(r < s.Length){
+            hash[s[r] - 'A']++;
+            maxFreq = Math.Max(maxFreq, hash[s[r] - 'A']);
+            if( (r-l+1) - maxFreq > k){
+                hash[s[l] - 'A']--; 
+                l++;
+            }
+            maxLen = Math.Max(maxLen, r - l + 1);
+            r++;
+        }
+        return maxLen;
+    }
+}
