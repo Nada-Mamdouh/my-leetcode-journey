452c5c108433d2f3893d72f57996022997513fe7
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-04-30 08:57:42 +0300

    Time: 1 ms (57.93%), Space: 42.7 MB (44.03%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cs
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cs
@@ -2,17 +2,16 @@ public class Solution {
     public int FindNumbers(int[] nums) {
         int cnt = 0;
         for(int i = 0;i<nums.Length;i++){
-            int len = getDigitsCount(nums[i]);
-            if(len % 2 == 0) cnt++;
+            if(hasEvenDigits(nums[i])) cnt++;
         }
         return cnt;
     }
-    int getDigitsCount(int num){
-        int len = 0;
+    bool hasEvenDigits(int num){
+        int digits = 0;
         while(num > 0){
-            len++;
+            digits++;
             num /= 10;
         }
-        return len;
+        return ((digits & 1) == 0);
     }
 }
