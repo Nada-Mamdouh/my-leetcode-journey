72b407bcd479ae03d8a603820cafb02f200f57ae
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-05-06 08:24:47 +0300

    Time: 3 ms (85.73%), Space: 60.1 MB (62.06%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cs
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cs
@@ -1,19 +1,15 @@
 public class Solution {
     public double FindMaxAverage(int[] nums, int k) {
-        int n = nums.Length;
-        double maxAvg = double.MinValue;
-        double windowAvg = 0; int windowSum = 0;
+        int n = nums.Length, windowSum = 0;
+        int maxSum = int.MinValue;
         for(int i = 0;i<k;i++){
             windowSum += nums[i];
         }
-        windowAvg = (windowSum*1.0) / (k*1.0);
-        maxAvg = windowAvg;
+        maxSum = windowSum;
         for(int i = k;i<n;i++){
-            windowSum -= nums[i-k];
-            windowSum += nums[i];
-            windowAvg = (windowSum*1.0) / (k*1.0);
-            maxAvg = Math.Max(maxAvg, windowAvg);
+            windowSum += nums[i] - nums[i-k];
+            maxSum = Math.Max(maxSum, windowSum);
         }
-        return maxAvg;
+        return (double) maxSum / (double) k;
     }
 }
