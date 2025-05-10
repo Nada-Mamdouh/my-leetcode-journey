cf65d78872264d1382bbed57e46828a2d4d657b3
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-05-10 12:07:09 +0300

    Time: 2 ms (91.21%), Space: 59.9 MB (13.84%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cs
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cs
@@ -1,23 +1,22 @@
 public class Solution {
     public int LongestOnes(int[] nums, int k) {
-        //Better solution
-        //Sliding window
-        //TC: O(2n) - SC: O(1)
-        int n = nums.Length, maxLen = 0, l = 0, r = 0, zeros = 0;
+        int n = nums.Length, zeroesCnt = 0;
+        int l = 0, r = 0, maxCons = 0, windowMax = 0;
         while(r < n){
-            if(nums[r] == 0) zeros++;
-            while(zeros > k){
-                if(nums[l] == 0) {
-                    zeros--;
-                }
+            if(nums[r] == 1) windowMax = r - l + 1;
+            if(nums[r] == 0 && zeroesCnt <= k){
+                zeroesCnt++;
+                windowMax = r - l + 1;
+            }
+            while(zeroesCnt > k){
+                if(nums[l] == 0) zeroesCnt--;
                 l++;
+                windowMax = r - l + 1;
             }
-            if(zeros <= k){
-                int len = r - l + 1;
-                maxLen = Math.Max(len, maxLen);
-            }
+
+            maxCons = Math.Max(maxCons, windowMax);
             r++;
         }
-        return maxLen;
+        return maxCons;
     }
 }
