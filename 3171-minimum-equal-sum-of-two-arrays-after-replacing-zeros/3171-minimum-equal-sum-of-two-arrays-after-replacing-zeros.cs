c9fdb9708426eb1b121e40202d951b88a31320b2
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-05-10 09:36:28 +0300

    Time: 483 ms (9.09%), Space: 68.9 MB (81.82%) - LeetHub

---
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public long MinSum(int[] nums1, int[] nums2) {
+        //TC: O(n+m)
+        int zeros1 = 0, zeros2 = 0;
+        long sum1 = 0, sum2 = 0;
+        foreach(var num in nums1){
+            sum1 += num;
+            if(num == 0){
+                zeros1++;
+                sum1++;
+            }
+        }
+        foreach(var num in nums2){
+            sum2 += num;
+            if(num == 0){
+                zeros2++;
+                sum2++;
+            }
+        }
+        if((zeros1 == 0 && sum2 > sum1) || (zeros2 == 0 && sum1 > sum2)) return -1;
+        return Math.Max(sum1, sum2);
+    }
+}
