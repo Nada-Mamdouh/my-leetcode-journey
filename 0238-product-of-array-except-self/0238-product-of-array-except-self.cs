6caa6b4087df19dd0b6bbd295ca574ff63088528
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-06-26 12:36:56 +0300

    Time: 2 ms (73.32%), Space: 67 MB (45.72%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -1,29 +1,22 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-        //TC: O(3n) - SC: O(n2)
-
-        // This problem using prefix & suffix algorithm but instead of prefix sum it's prefix
-        // multiplication
-        // If we used division that would be easy one 
-        // ---------------------------
-        // 1- Create Prefix array:
+        //TC:O(2n) - SC: O(1)
+        //Trying the follow-up - cheating from top beats 
         int n = nums.Length;
-        int[] prefix = new int[n];
-        prefix[0] = nums[0];
-        for(int i = 1;i<n;i++) prefix[i] = prefix[i-1] * nums[i];
-
-        //2- Create suffix array:
-        int[] suffix = new int[n];
-        suffix[n-1] = nums[n-1];
-        for(int i = n-2;i>=0;i--) suffix[i] = suffix[i+1] * nums[i];
-
-        //3- Getting / populating the answer
         int[] ans = new int[n];
+        int prod = 1;
+        //1- Going from right to left - right sum/mult
         for(int i = 0;i<n;i++){
-            if(i == 0) ans[i] = suffix[i+1];
-            else if(i == n-1) ans[i] = prefix[i-1];
-            else ans[i] = prefix[i-1] * suffix[i+1];
+            ans[i] = prod;
+            prod *= nums[i];
+        }
+        //2- Going from left to right - left sum/mult
+        prod = 1;
+        for(int i = n-1;i>=0;i--){
+            ans[i] = ans[i] * prod;
+            prod *= nums[i];
         }
         return ans;
+        
     }
 }
