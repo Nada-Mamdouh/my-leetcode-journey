2529d9296bf5d87c37057a4775078a9a1a4b8a79
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-07-18 00:31:28 +0300

    Time: 18 ms (61.46%), Space: 44.9 MB (79.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -1,16 +1,16 @@
-using System.Text.RegularExpressions;
-
 public class Solution {
     public bool IsPalindrome(string s) {
-        int start = 0, end = s.Length -1;
-        while(start<end){
-            if(!char.IsLetterOrDigit(s[start])) start++;
-            else if(!char.IsLetterOrDigit(s[end])) end--;
-            else{
-                if(char.ToLower(s[start++]) != char.ToLower(s[end--])) return false;
-            }
+        //chars 97 - 122 - nums: 48 - 57
+        int start = 0, end = s.Length - 1;
+        string sLowered = s.ToLower();
+        while(start < end){
+            while(start < s.Length-1 && !isAlphaNumeric(sLowered[start]))start++;
+            while(end > 1 && !isAlphaNumeric(sLowered[end])) end--;
+            if(isAlphaNumeric(sLowered[start]) && isAlphaNumeric(sLowered[end]) && sLowered[start] != sLowered[end]) return false;
+            start++;
+            end--;
         }
-        
         return true;
     }
+    bool isAlphaNumeric(char c) => (c >= 48 && c <= 57) || (c >= 97 && c <= 122);
 }
