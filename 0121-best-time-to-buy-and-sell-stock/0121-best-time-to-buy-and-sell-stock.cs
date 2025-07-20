c8c5240e9a341c909a450d95ce81fa5eedd107fb
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-07-20 21:55:53 +0300

    Time: 2 ms (46.93%), Space: 60.2 MB (25.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -1,10 +1,10 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int profit = 0, n = prices.Length;
-        int buyingPrice = prices[0];
-        for(int i = 0;i<n;i++){
-            if(prices[i] > buyingPrice) profit = Math.Max(profit, prices[i] - buyingPrice);
-            else buyingPrice = prices[i];
+        int minPrice = int.MaxValue;
+        int profit = 0;
+        for(int i = 0;i<prices.Length;i++){
+            minPrice = Math.Min(minPrice, prices[i]);
+            profit = Math.Max(profit, prices[i] - minPrice);
         }
         return profit;
     }
