988d40e2ed99e94c3ed5897ea5cd0bb05beba7c4
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-07-29 22:31:51 +0300

    Time: 0 ms (100%), Space: 31.3 MB (9.43%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cs
+++ 0231-power-of-two/0231-power-of-two.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public bool IsPowerOfTwo(int n) {
-        //Iterative approach
-        if(n == 0) return false;
+        return isPow((double)n);
+    }
+    bool isPow(double n){
+        if(n <= 0) return false;
         if(n == 1) return true;
-        while(n % 2 == 0){
-            if(n == 2) return true;
-            n /= 2;
-        }
-        return false;
+        if(n % 2 != 0) return false;
+        return isPow(n/2);
     }
 }
