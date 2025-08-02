fa06c86edb7e59ff1618ed74200c1943cc786c54
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-08-02 22:12:21 +0300

    Time: 0 ms (100%), Space: 46.9 MB (65.98%) - LeetHub

--- 0078-subsets/0078-subsets.cs
+++ 0078-subsets/0078-subsets.cs
@@ -1,18 +1,19 @@
 public class Solution {
     public IList<IList<int>> Subsets(int[] nums) {
         var result = new List<IList<int>>();
-        Backtrack(nums,0,new List<int>(),result);
+        Backtrack(nums, 0, new List<int>(), result);
         return result;
     }
-    void Backtrack(int[] nums, int start, List<int> current, List<IList<int>> result) {
-        // Add a copy of the current subset
-        result.Add(new List<int>(current));
-
-        // Explore further by including nums[i]
-        for (int i = start; i < nums.Length; i++) {
-            current.Add(nums[i]); // choose
-            Backtrack(nums, i + 1, current, result); // explore
-            current.RemoveAt(current.Count - 1); // un-choose (backtrack)
+    void Backtrack(int[] nums, int idx, List<int> current,List<IList<int>> result){
+        if(idx == nums.Length){
+            result.Add(new List<int>(current));
+            return;
         }
+
+        current.Add(nums[idx]);
+        Backtrack(nums,idx+1,current,result);
+        current.RemoveAt(current.Count - 1);
+        Backtrack(nums, idx+1,current,result);
     }
+    
 }
