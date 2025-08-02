81d9236596b1b630bc5294ad416e05a24511b76c
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-08-02 19:39:30 +0300

    Time: 21 ms (6.58%), Space: 41.8 MB (94.74%) - LeetHub

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cs
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cs
@@ -1,25 +1,13 @@
 public class Solution {
     public int ArithmeticTriplets(int[] nums, int diff) {
-        //Extreme brute force approach
-        //TC: O(n^3)
-        //SC: O(1)
-        int countOfTriplets = 0;
-        int n = nums.Length;
-        for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                for(int k = j + 1;k<n;k++){
-                    if(k > j && j > i){
-                        int diff1 = nums[k] - nums[j];
-                        int diff2 = nums[j] - nums[i];
-                        if(diff1 == diff && diff2 == diff) {
-                            countOfTriplets++;
-                            break;
-                        }
-                    }
+        int ans = 0;
+        for(int i = 0;i<nums.Length - 2;i++){
+            for(int j = i + 1;j<nums.Length -1;j++){
+                for(int k = j + 1;k<nums.Length;k++){
+                    if(k > j && j > i && nums[k] - nums[j] == diff && nums[j] - nums[i] == diff) ans++;
                 }
             }
-            
         }
-        return countOfTriplets;
+        return ans;
     }
 }
