da755b29910c8eb45d9f929fda4a828f482bc9db
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-08-04 22:40:26 +0300

    Time: 0 ms (100%), Space: 47.1 MB (50.88%) - LeetHub

--- 0078-subsets/0078-subsets.cs
+++ 0078-subsets/0078-subsets.cs
@@ -1,19 +1,18 @@
 public class Solution {
     public IList<IList<int>> Subsets(int[] nums) {
-        var result = new List<IList<int>>();
-        Backtrack(nums, 0, new List<int>(), result);
-        return result;
+       var result = new List<IList<int>>();
+       Backtrack(nums, 0, new List<int>(), result);
+       return result;
     }
     void Backtrack(int[] nums, int idx, List<int> current,List<IList<int>> result){
         if(idx == nums.Length){
             result.Add(new List<int>(current));
             return;
         }
-
         current.Add(nums[idx]);
         Backtrack(nums,idx+1,current,result);
         current.RemoveAt(current.Count - 1);
-        Backtrack(nums, idx+1,current,result);
+        Backtrack(nums,idx+1,current,result);
     }
     
 }
