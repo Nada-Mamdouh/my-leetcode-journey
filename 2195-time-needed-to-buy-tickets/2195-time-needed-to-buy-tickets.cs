66f3c90726f59b226fe51e1e8a3a2b7b6c8e6b11
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-08-06 23:20:41 +0300

    Time: 0 ms (100%), Space: 41.1 MB (98.41%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cs
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cs
@@ -1,19 +1,10 @@
 public class Solution {
     public int TimeRequiredToBuy(int[] tickets, int k) {
-        Queue<int> q = new();
-        int times = 0;
+        int ans = 0;
         for(int i = 0;i<tickets.Length;i++){
-            q.Enqueue(i);
+            if(i <= k) ans += Math.Min(tickets[i],tickets[k]);
+            if(i > k) ans += Math.Min(tickets[i], tickets[k]-1);
         }
-        while(q.Count > 0){
-            int front = q.Dequeue();
-            times++;
-            tickets[front]--;
-
-            if(front == k && tickets[front] == 0) return times;
-            if(tickets[front] == 0) continue;
-            q.Enqueue(front);
-        }
-        return times;
+        return ans;
     }
 }
