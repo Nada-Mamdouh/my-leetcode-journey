63a9513b7c2532e714f73f5eb16526004ebfa0b3
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-08-21 23:32:49 +0300

    Time: 1 ms (82.88%), Space: 47.8 MB (33.51%) - LeetHub

--- 0046-permutations/0046-permutations.cs
+++ 0046-permutations/0046-permutations.cs
@@ -1,23 +1,28 @@
 public class Solution {
+    //Another solution - no extra map
     IList<IList<int>> res = new List<IList<int>>();
     public IList<IList<int>> Permute(int[] nums) {
-        GeneratePermutations(ref nums, new List<int>(), new bool[nums.Length]);
+        GeneratePermutations(ref nums, 0);
         return res;
     }
-    void GeneratePermutations(ref int[] nums, List<int> ds, bool[] freq){
-        if(ds.Count == nums.Length){
+    void GeneratePermutations(ref int[] nums, int idx){
+        if(idx == nums.Length){
+            List<int> ds = new();
+            for(int i = 0;i<nums.Length;i++){
+                ds.Add(nums[i]);
+            }
             res.Add(new List<int>(ds));
             return;
         }
-        for(int i = 0;i<nums.Length;i++){
-            if(!freq[i]){
-                freq[i] = true;
-                ds.Add(nums[i]);
-                GeneratePermutations(ref nums, ds, freq);
-
-                ds.RemoveAt(ds.Count - 1);
-                freq[i] = false;
-            }
+        for(int i = idx;i<nums.Length;i++){
+            swap(ref nums, i, idx);
+            GeneratePermutations(ref nums, idx + 1);
+            swap(ref nums , i , idx);
         }
+    }
+    void swap(ref int[] nums, int i , int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
