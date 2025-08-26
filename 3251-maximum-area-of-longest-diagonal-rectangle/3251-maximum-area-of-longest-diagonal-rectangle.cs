6b9a23944d7f387a746e66b2f32994a3ba5e5806
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-08-26 23:24:40 +0300

    Time: 0 ms (100%), Space: 45.7 MB (95.65%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cs
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cs
@@ -1,28 +1,22 @@
 public class Solution {
     public int AreaOfMaxDiagonal(int[][] dimensions) {
-        Dictionary<double,List<double>> dict = new();
-        double maxDiagonal = 0;
-
+        double maxDiagonal = 0; int maxArea = 0;
         for(int i = 0;i<dimensions.Length;i++){
-            double length =(double) dimensions[i][0];
-            double width = (double) dimensions[i][1];
-            double diagonal = Math.Sqrt( (length * length) + (width * width));
+            int length = dimensions[i][0];
+            int width = dimensions[i][1];
 
-            if(diagonal > maxDiagonal){
-                maxDiagonal = diagonal;
+            var currD = Math.Sqrt(length * length + width * width);
+            var currArea = length * width;
+
+            if(currD > maxDiagonal){
+                maxDiagonal = currD;
+                maxArea = currArea;
             }
 
-            if(!dict.ContainsKey(diagonal)){
-                dict.Add(diagonal, new List<double>());
+            if(currD == maxDiagonal){
+                maxArea = Math.Max(maxArea, currArea);
             }
-            dict[diagonal].Add((double)(length * width));
             
-        }
-
-        if(dict[maxDiagonal].Count == 1) return (int) dict[maxDiagonal][0];
-        int maxArea = 0; 
-        foreach(var area in dict[maxDiagonal]){
-            maxArea = Math.Max(maxArea, (int) area);
         }
         return maxArea;
     }
