6ecb0472e8db18551cc81ae940eecc0f52ad4241
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-08-31 02:23:56 +0300

    Time: 2410 ms (5.28%), Space: 62.8 MB (7.04%) - LeetHub

---
+++ 0838-design-linked-list/0838-design-linked-list.cs
@@ -0,0 +1,70 @@
+public class MyLinkedList {
+    int[] arr;
+    int size;
+    int capacity = 1;
+    public MyLinkedList() {
+        arr = new int[capacity];
+        size = 0;
+    }
+    
+    public int Get(int index) {
+        for(int i = 0;i<size;i++){
+            Console.WriteLine(arr[i]);
+        }
+        Console.WriteLine(size + " " + index);
+        if(size == 0 || index >= size) return -1;
+        return arr[index];
+    }
+    
+    public void AddAtHead(int val) {
+        AddAtIndex(0, val);
+    }
+    
+    public void AddAtTail(int val) {
+        AddAtIndex(size, val);
+    }
+    
+    public void AddAtIndex(int index, int val) {
+        if(size == capacity) resize(capacity * 2);
+        if(index < capacity){
+            for(int i = size;i > index;i--){
+                arr[i] = arr[i-1];
+            }
+            Console.WriteLine("Position "+ index + " before inserting"+arr[index]);
+            arr[index] = val;
+            Console.WriteLine("Position "+ index + " after inserting"+arr[index]);
+            size++;
+        }
+    }
+    
+    public void DeleteAtIndex(int index) {
+        if(index >= 0 && index < size){
+            for(int i = index; i<size-1;i++){
+                arr[i] = arr[i+1];
+            }
+            Console.WriteLine("After delete " + arr[index]);
+            size--;
+            if(size != 0 && capacity > 1 && size == capacity/4) resize(capacity / 2);
+        }
+        
+    }
+    void resize(int newCapacity){
+        int[] newArr = new int[newCapacity];
+        for(int i = 0;i<size;i++){
+            newArr[i] = arr[i];
+        }
+        capacity = newCapacity;
+        arr = newArr;
+    }
+    private bool IsEmpty() => size == 0;
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.Get(index);
+ * obj.AddAtHead(val);
+ * obj.AddAtTail(val);
+ * obj.AddAtIndex(index,val);
+ * obj.DeleteAtIndex(index);
+ */
