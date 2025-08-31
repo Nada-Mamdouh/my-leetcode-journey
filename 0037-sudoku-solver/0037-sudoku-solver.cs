4291a97d0347b1b9a2c48e428e18fc544831227c
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-08-31 22:34:09 +0300

    Time: 1179 ms (38.05%), Space: 47.9 MB (74.28%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public void SolveSudoku(char[][] board) {
+        solve(ref board);
+    }
+    bool solve(ref char[][] board){
+        for(int i = 0;i<9;i++){
+            for(int j = 0;j<9;j++){
+                if(board[i][j] == '.'){
+                    for(char c = '1' ; c <= '9';c++){
+                        if(isValid(ref board, i, j, c)){
+                            board[i][j] = c;
+                            if(solve(ref board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isValid(ref char[][] board, int row , int col, char c){
+        for(int i = 0;i <9;i++){
+            if(board[row][i] == c) return false;
+            if(board[i][col] == c) return false;
+            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
