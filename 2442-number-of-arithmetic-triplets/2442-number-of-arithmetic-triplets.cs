77c3fbf7ff483b9d403753958c67b1d611835996
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-09-07 21:55:00 +0300

    Time: 3 ms (37.35%), Space: 42.5 MB (25.3%) - LeetHub

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cs
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public int ArithmeticTriplets(int[] nums, int diff) {
         int ans = 0;
-        for(int i = 0;i<nums.Length - 2;i++){
-            for(int j = i + 1;j<nums.Length -1;j++){
-                for(int k = j + 1;k<nums.Length;k++){
-                    if(k > j && j > i && nums[k] - nums[j] == diff && nums[j] - nums[i] == diff) ans++;
-                }
-            }
+        HashSet<int> sett = new HashSet<int>();
+        for(int i = 0;i<nums.Length;i++){
+            if(sett.Contains(nums[i] - diff) && sett.Contains(nums[i] - 2 * diff))
+            ans++;
+            sett.Add(nums[i]);
         }
         return ans;
     }
