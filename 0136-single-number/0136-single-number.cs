547156404d8589fdf038fe3bb31422cb0989e607
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-09-23 00:08:47 +0300

    Time: 0 ms (100%), Space: 46.2 MB (95.23%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,8 +1,6 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        Array.Sort(nums);
         int ans = 0;
-        bool isUnique = true;
         for(int i = 0;i<nums.Length;i++){
             ans ^= nums[i];
         }
