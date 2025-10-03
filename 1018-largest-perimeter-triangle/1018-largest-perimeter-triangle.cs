b27288d4cac76e5f9c0dc9872842c828e748360f
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-10-03 23:11:10 +0300

    Time: 41 ms (9.05%), Space: 49.8 MB (40.51%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cs
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cs
@@ -1,9 +1,9 @@
 public class Solution {
     public int LargestPerimeter(int[] A) {
         Array.Sort(A);
-        for (int i = A.Length - 3; i >= 0; --i)
-            if (A[i] + A[i+1] > A[i+2])
-                return A[i] + A[i+1] + A[i+2];
+        for(int i = A.Length - 3;i>=0;i--){
+            if(A[i] + A[i+1] > A[i+2]) return A[i] + A[i+1] + A[i+2];
+        }
         return 0;
     }
 }
