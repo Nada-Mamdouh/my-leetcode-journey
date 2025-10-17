8fa02070ea9b8f40681e0c103e89522d3d80fa75
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-10-18 00:05:43 +0300

    Time: 2 ms (99.25%), Space: 44.9 MB (94.08%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public string ReverseVowels(string s) {
+        int n = s.Length;
+        int j = 0, k = n-1;
+        var sArr = s.ToCharArray();
+        while(j < n && k > 0 && j < k){
+            if(IsVowel(s[j]) && IsVowel(s[k])){
+                sArr[j] = s[k];
+                sArr[k] = s[j];
+                j++;
+                k--;
+            }else if(IsVowel(s[j])) k--;
+            else j++;
+        }
+        return new String(sArr);
+    }
+    bool IsVowel(char c)=> c == 'a' || c == 'e' || c == 'i' || c == 'o' || c== 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+}
