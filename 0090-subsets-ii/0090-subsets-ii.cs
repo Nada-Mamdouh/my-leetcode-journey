f47d2382d35e418cec98581179acd187893c512e
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-11-14 19:04:00 +0200

    Time: 2 ms (72.39%), Space: 47.2 MB (74.23%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cs
+++ 0090-subsets-ii/0090-subsets-ii.cs
@@ -1,26 +1,18 @@
 public class Solution {
     IList<IList<int>> ans = new List<IList<int>>();
     public IList<IList<int>> SubsetsWithDup(int[] nums) {
-        //1- sort 
         Array.Sort(nums);
-        //2- find subsets
-        FindSubSets(ref nums, 0, new List<int>());
+        BackTrack(ref nums, 0, new List<int>());
         return ans;
     }
-    void FindSubSets(ref int[] nums, int idx, List<int> ds){
-        //3- add subsets 
-        ans.Add(new List<int>(ds));
-        for(int i = idx;i<nums.Length;i++){
-            //4- check for duplicates on the same recursion level
-            if(i > idx && nums[i] == nums[i-1]) continue;
 
-            //5- pick 
-            ds.Add(nums[i]);
-            FindSubSets(ref nums, i + 1, ds);
-            //6- not pick 
-            ds.RemoveAt(ds.Count - 1);
-            
+    void BackTrack(ref int[] nums, int idx, List<int> lst){
+        ans.Add(new List<int>(lst));
+        for(int i = idx; i<nums.Length;i++){
+            if(i > idx && nums[i] == nums[i-1]) continue;
+            lst.Add(nums[i]);
+            BackTrack(ref nums, i + 1, lst);
+            lst.RemoveAt(lst.Count - 1);
         }
     }
-
 }
