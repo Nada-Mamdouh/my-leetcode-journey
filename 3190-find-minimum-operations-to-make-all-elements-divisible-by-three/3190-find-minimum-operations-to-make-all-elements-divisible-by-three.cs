d63ec2faa678ec6b979ea81ac30045e20c66edc7
Nada Mamdouh <nada-mamdouh@outlook.com>
2025-11-23 00:18:25 +0200

    Time: 0 ms (100%), Space: 43 MB (50%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cs
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cs
@@ -1,11 +1,8 @@
 public class Solution {
     public int MinimumOperations(int[] nums) {
-        int operationsNum = 0;
-        for(int i = 0; i<nums.Length;i++){
-            if(((nums[i]-1) % 3 == 0) || ((nums[i]+1) % 3 == 0)){
-                operationsNum++;
-            }
-        }
-        return operationsNum;
+        int opCount = 0;
+        for(int i = 0;i<nums.Length;i++)
+            if(nums[i] % 3 != 0) opCount++;
+        return opCount;
     }
 }
