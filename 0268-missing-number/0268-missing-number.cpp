f6a7192367fcf89be5e1bf1d045965fe5ffacd16
Nada Mamdouh <nada-mamdouh@outlook.com>
2026-03-21 22:32:33 +0200

    Time: 11 ms (10.55%), Space: 21.8 MB (37.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        //Optimal approach
-        //TC(O(n))   SC: O(1)
-        //Using sum
-        int n = nums.size();
-        int sum1 = n * (n+1) / 2, s2 = 0;
-        for(int i = 0;i<n;i++){
-            s2 += nums[i];
+        //O(nlogn)
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<nums.size();i++){
+            if(i != nums[i]) return i;
         }
-        return sum1 - s2;
+        return nums.size();
     }
 };
