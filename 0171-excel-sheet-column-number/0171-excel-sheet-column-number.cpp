9531295b7056072fa4bd885960966d0804b70cb7
Nada Mamdouh <nada-mamdouh@outlook.com>
2026-03-26 22:53:35 +0200

    Time: 0 ms (100%), Space: 8.6 MB (75.14%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -3,7 +3,7 @@ public:
     int titleToNumber(string columnTitle) {
         int ans = 0, n = columnTitle.size();
         for(int i = 0;i<n;i++){
-            ans = ans * 26 + (columnTitle[i] - 64);
+            ans = ans * 26 + (columnTitle[i] - 'A' + 1);
         }
         return ans;
     }
