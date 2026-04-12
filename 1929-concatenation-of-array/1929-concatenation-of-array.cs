e7b1ffa808b936bd75c883c54fcb0ce8aefa8be8
Nada Mamdouh <nada-mamdouh@outlook.com>
2026-04-12 20:20:44 +0200

    Time: 2 ms (10.5%), Space: 53.3 MB (6.74%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -1,11 +1,12 @@
 public class Solution {
     public int[] GetConcatenation(int[] nums) {
         int n = nums.Length;
-        int[] ans = new int[n * 2];
+        List<int> ans = new List<int>();
+        for(int i = 0;i<2 * n;i++) ans.Add(0);
         for(int i = 0;i<n;i++){
             ans[i] = nums[i];
-            ans[i+n] = nums[i];
+            ans[i + n] = nums[i];
         }
-        return ans;
+        return ans.ToArray();
     }
 }
