9b950d19731116336c8810d8eded0193ea8766fd
Nada Mamdouh <nada-mamdouh@outlook.com>
2026-05-09 22:24:23 +0300

    Time: 0 ms (100%), Space: 13.6 MB (41.55%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        while(left <= right){
-            int mid = (left + right) / 2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] > target) right = mid - 1;
-            else left = mid + 1;
-        }
-        return left;
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
     }
 };
